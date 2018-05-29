94dddc5536a5a594bc6fc56453c742871ffa6288
MelloFelipe <39704657+MelloFelipe@users.noreply.github.com>
2018-05-29 11:42:21 -0300

    Update UC.vhd

--- UC.vhd
+++ UC.vhd
@@ -1,58 +1,381 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY UC IS
-	PORT (
-		clock			: IN	STD_LOGIC;
-		key_on		: IN	STD_LOGIC_VECTOR (2  DOWNTO 0);
-   	key_code	   : IN	STD_LOGIC_VECTOR (47 DOWNTO 0);
-		--IR_Ld, PC_Inc, ALU_2_DBus, DM_Rd, DM_Wr, PC_Ld_En, Reg_2_IO, IO_2_Reg, Reg_Wr, Stat_Wr, DM_2_DBus: OUT	STD_LOGIC		
-	);
-END ENTITY;
+entity UC is
+  port (    
+    CLOCK_50                  : in  std_logic;
+    --KEY                       : in  std_logic_vector(0 downto 0);
+	 key_on							: in	std_logic_vector(2 downto 0);
+	 key_code						: in	std_logic_vector(47 downto 0);
+    VGA_R, VGA_G, VGA_B       : out std_logic_vector(7 downto 0);
+    VGA_HS, VGA_VS            : out std_logic;
+    VGA_BLANK_N, VGA_SYNC_N   : out std_logic;
+    VGA_CLK                   : out std_logic
+    );
+end UC;
 
-ARCHITECTURE Behavior OF UC IS
-
-	TYPE states_UC IS (INICIO, INICIO_PARTIDA, PARTIDA, GAME_OVER);
-	SIGNAL current_state : states_UC := INICIO;
+architecture comportamento of UC is
   
-BEGIN
-	StateMachine:
-	PROCESS (clock, key_on, current_state)
-	BEGIN
-		IF (clock'EVENT AND clock = '1') THEN
-			-- Talvez zerar alguns sinais aqui
-			
-			CASE current_state IS
-			
-				WHEN INICIO =>
-					IF key_on(0) = '1' THEN
-						IF key_code(15 downto 0) = "005A" THEN-- Enter
-							current_state <= INICIO_PARTIDA;
-						END IF;
-					END IF;
-          
-				WHEN INICIO_PARTIDA =>
-          
-				WHEN PARTIDA =>
-					CASE key_on IS
-						WHEN "000" =>
+  signal rstn : std_logic;              -- reset active low para nossos
+                                        -- circuitos sequenciais.
 
-						WHEN "001" =>
+  -- Interface com a memória de vídeo do controlador
 
-						WHEN "010" =>
+  signal we : std_logic;                        -- write enable ('1' p/ escrita)
+  signal addr : integer range 0 to 12287;       -- endereco mem. vga
+  signal pixel : std_logic_vector(2 downto 0);  -- valor de cor do pixel
+  signal pixel_bit : std_logic;                 -- um bit do vetor acima
 
-						WHEN "011" =>
+  -- Sinais dos contadores de linhas e colunas utilizados para percorrer
+  -- as posições da memória de vídeo (pixels) no momento de construir um quadro.
+  
+  signal line : integer range 0 to 95;  -- linha atual
+  signal col : integer range 0 to 127;  -- coluna atual
 
-						WHEN "100" =>
+  signal col_rstn : std_logic;          -- reset do contador de colunas
+  signal col_enable : std_logic;        -- enable do contador de colunas
 
-						WHEN "101" =>
+  signal line_rstn : std_logic;          -- reset do contador de linhas
+  signal line_enable : std_logic;        -- enable do contador de linhas
 
-						WHEN "110" =>
+  signal fim_escrita : std_logic;       -- '1' quando um quadro terminou de ser
+                                        -- escrito na memória de vídeo
 
-						WHEN OTHERS =>
+  -- Sinais que armazem a posição de uma bola, que deverá ser desenhada
+  -- na tela de acordo com sua posição.
 
-					END CASE;			
-			END CASE;
-		END IF;
-	END PROCESS;
-END ARCHITECTURE;
+  signal pos_bola_x : integer range 0 to 127;  -- coluna atual da bola
+  signal pos_bola_y : integer range 0 to 95;   -- linha atual da bola
+
+  signal atualiza_pos_bola_x : std_logic;    -- se '1' = bola muda sua pos. no eixo x
+  signal atualiza_pos_bola_y : std_logic;    -- se '1' = bola muda sua pos. no eixo y
+
+  -- Especificação dos tipos e sinais da máquina de estados de controle
+  type estado_t is (inicio_jogo, inicio_partida, constroi_quadro, move_bola);
+  signal estado: estado_t := inicio_jogo;
+  signal proximo_estado: estado_t := inicio_jogo;
+
+  -- Sinais para um contador utilizado para atrasar a atualização da
+  -- posição da bola, a fim de evitar que a animação fique excessivamente
+  -- veloz. Aqui utilizamos um contador de 0 a 1250000, de modo que quando
+  -- alimentado com um clock de 50MHz, ele demore 25ms (40fps) para contar até o final.
+  
+  signal contador : integer range 0 to 1250000 - 1;  -- contador
+  signal timer : std_logic;        -- vale '1' quando o contador chegar ao fim
+  signal timer_rstn, timer_enable : std_logic;
+  
+  signal sync, blank: std_logic;
+
+  -- Sinais para controlar o movimento dos PAD's
+
+  signal pos_PAD1 : integer range 0 to 95;   -- linha atual do pixel central do PAD1
+  signal pos_PAD2 : integer range 0 to 95;   -- linha atual do pixel central do PAD2
+
+  signal atualiza_pos_PAD1 : std_logic;    -- se '1' = PAD1 muda sua pos. no eixo y
+  signal atualiza_pos_PAD2 : std_logic;    -- se '1' = PAD2 muda sua pos. no eixo y
+  
+  -- O QUE FALTA:
+  -- vgaball sempre atualiza a posicao x e y ao mesmo tempo, ou seja, a bola nunca
+  -- vai perfeitamente na horizontal.
+begin  -- comportamento
+
+
+  -- Aqui instanciamos o controlador de vídeo, 128 colunas por 96 linhas
+  -- (aspect ratio 4:3). Os sinais que iremos utilizar para comunicar
+  -- com a memória de vídeo (para alterar o brilho dos pixels) são
+  -- write_clk (nosso clock), write_enable ('1' quando queremos escrever
+  -- o valor de um pixel), write_addr (endereço do pixel a escrever)
+  -- e data_in (valor do brilho do pixel RGB, 1 bit pra cada componente de cor)
+  vga_controller: entity work.vgacon port map (
+    clk50M       => CLOCK_50,
+    rstn         => '1',
+    red          => VGA_R,
+    green        => VGA_G,
+    blue         => VGA_B,
+    hsync        => VGA_HS,
+    vsync        => VGA_VS,
+    write_clk    => CLOCK_50,
+    write_enable => we,
+    write_addr   => addr,
+    data_in      => pixel,
+    vga_clk      => VGA_CLK,
+    sync         => sync,
+    blank        => blank);
+  VGA_SYNC_N <= NOT sync;
+  VGA_BLANK_N <= NOT blank;
+
+  -----------------------------------------------------------------------------
+  -- Processos que controlam contadores de linhas e coluna para varrer
+  -- todos os endereços da memória de vídeo, no momento de construir um quadro.
+  -----------------------------------------------------------------------------
+
+  -- purpose: Este processo conta o número da coluna atual, quando habilitado
+  --          pelo sinal "col_enable".
+  -- type   : sequential
+  -- inputs : CLOCK_50, col_rstn
+  -- outputs: col
+  conta_coluna: process (CLOCK_50, col_rstn)
+  begin  -- process conta_coluna
+    if col_rstn = '0' then                  -- asynchronous reset (active low)
+      col <= 0;
+    elsif CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
+      if col_enable = '1' then
+        if col = 127 then               -- conta de 0 a 127 (128 colunas)
+          col <= 0;
+        else
+          col <= col + 1;  
+        end if;
+      end if;
+    end if;
+  end process conta_coluna;
+    
+  -- purpose: Este processo conta o número da linha atual, quando habilitado
+  --          pelo sinal "line_enable".
+  -- type   : sequential
+  -- inputs : CLOCK_50, line_rstn
+  -- outputs: line
+  conta_linha: process (CLOCK_50, line_rstn)
+  begin  -- process conta_linha
+    if line_rstn = '0' then                  -- asynchronous reset (active low)
+      line <= 0;
+    elsif CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
+      -- o contador de linha só incrementa quando o contador de colunas
+      -- chegou ao fim (valor 127)
+      if line_enable = '1' and col = 127 then
+        if line = 95 then               -- conta de 0 a 95 (96 linhas)
+          line <= 0;
+        else
+          line <= line + 1;  
+        end if;        
+      end if;
+    end if;
+  end process conta_linha;
+
+  -- Este sinal é útil para informar nossa lógica de controle quando
+  -- o quadro terminou de ser escrito na memória de vídeo, para que
+  -- possamos avançar para o próximo estado.
+  fim_escrita <= '1' when (line = 95) and (col = 127)
+                 else '0'; 
+
+  -----------------------------------------------------------------------------
+  -- Abaixo estão processos relacionados com a atualização da posição da
+  -- bola. Todos são controlados por sinais de enable de modo que a posição
+  -- só é de fato atualizada quando o controle (uma máquina de estados)
+  -- solicitar.
+  -----------------------------------------------------------------------------
+
+  -- purpose: Este processo irá atualizar a coluna atual da bola,
+  --          alterando sua posição no próximo quadro a ser desenhado.
+  -- type   : sequential
+  -- inputs : CLOCK_50, rstn
+  -- outputs: pos_bola_x
+  p_atualiza_pos_bola_x: process (CLOCK_50, rstn)
+    type direcao_t is (direita, esquerda);
+    variable direcao : direcao_t := direita;
+  begin  -- process p_atualiza_pos_bola_x
+    if rstn = '0' then                  -- asynchronous reset (active low)
+      pos_bola_x <= 0;
+    elsif CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
+      if atualiza_pos_bola_x = '1' then
+        if direcao = direita then         
+          if pos_bola_x = 127 then -- BOLA CHEGOU NA FRONTEIRA
+				-- TEMOS QUE FAZER ALGUM CÁLCULO AQUI
+            direcao := esquerda;  
+          else
+            pos_bola_x <= pos_bola_x + 1;
+          end if;        
+        else  -- se a direcao é esquerda
+          if pos_bola_x = 0 then
+            direcao := direita;
+          else
+            pos_bola_x <= pos_bola_x - 1;
+          end if;
+        end if;
+      end if;
+    end if;
+  end process p_atualiza_pos_bola_x;
+
+  -- purpose: Este processo irá atualizar a linha atual da bola,
+  --          alterando sua posição no próximo quadro a ser desenhado.
+  -- type   : sequential
+  -- inputs : CLOCK_50, rstn
+  -- outputs: pos_bola_y
+  p_atualiza_pos_bola_y: process (CLOCK_50, rstn)
+    type direcao_t is (desce, sobe);
+    variable direcao : direcao_t := desce;
+  begin  -- process p_atualiza_pos_bola_x
+    if rstn = '0' then                  -- asynchronous reset (active low)
+      pos_bola_y <= 0;
+    elsif CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
+      if atualiza_pos_bola_y = '1' then
+        if direcao = desce then         
+          if pos_bola_y = 95 then
+            direcao := sobe;  
+          else
+            pos_bola_y <= pos_bola_y + 1;
+          end if;        
+        else  -- se a direcao é para subir
+          if pos_bola_y = 0 then
+            direcao := desce;
+          else
+            pos_bola_y <= pos_bola_y - 1;
+          end if;
+        end if;
+      end if;
+    end if;
+  end process p_atualiza_pos_bola_y;
+
+  -----------------------------------------------------------------------------
+  -- Brilho do pixel
+  -----------------------------------------------------------------------------
+  -- O brilho do pixel é branco quando os contadores de linha e coluna, que
+  -- indicam o endereço do pixel sendo escrito para o quadro atual, casam com a
+  -- posição da bola (sinais pos_bola_x e pos_bola_y). Caso contrário,
+  -- o pixel é preto.
+
+  pixel_bit <= '1' when (col = pos_bola_x) and (line = pos_bola_y) else '0';
+  pixel <= (others => pixel_bit);
+  
+  -- O endereço de memória pode ser construído com essa fórmula simples,
+  -- a partir da linha e coluna atual
+  addr  <= col + (128 * line);
+
+  -----------------------------------------------------------------------------
+  -- Processos que definem a FSM (finite state machine), nossa máquina
+  -- de estados de controle.
+  -----------------------------------------------------------------------------
+
+  -- purpose: Esta é a lógica combinacional que calcula sinais de saída a partir
+  --          do estado atual e alguns sinais de entrada (Máquina de Mealy).
+  -- type   : combinational
+  -- inputs : estado, fim_escrita, timer
+  -- outputs: proximo_estado, atualiza_pos_bola_x, atualiza_pos_bola_y, line_rstn,
+  --          line_enable, col_rstn, col_enable, we, timer_enable, timer_rstn
+  logica_mealy: process (estado, fim_escrita, timer)
+  begin  -- process logica_mealy
+    case estado is
+      when inicio_partida         => if timer = '1' then              
+                               proximo_estado <= constroi_quadro;
+                             else
+                               proximo_estado <= inicio_partida;
+                             end if;
+                             atualiza_pos_bola_x <= '0';
+                             atualiza_pos_bola_y <= '0';
+                             line_rstn      <= '0';  -- reset é active low!
+                             line_enable    <= '0';
+                             col_rstn       <= '0';  -- reset é active low!
+                             col_enable     <= '0';
+                             we             <= '0';
+                             timer_rstn     <= '1';  -- reset é active low!
+                             timer_enable   <= '1';
+
+      when constroi_quadro=> if fim_escrita = '1' then
+                               proximo_estado <= move_bola;
+                             else
+                               proximo_estado <= constroi_quadro;
+                             end if;
+                             atualiza_pos_bola_x <= '0';
+                             atualiza_pos_bola_y <= '0';
+                             line_rstn      <= '1';
+                             line_enable    <= '1';
+                             col_rstn       <= '1';
+                             col_enable     <= '1';
+                             we             <= '1';
+                             timer_rstn     <= '0'; 
+                             timer_enable   <= '0';
+
+      when move_bola      => proximo_estado <= inicio_partida;
+                             atualiza_pos_bola_x <= '1';
+                             atualiza_pos_bola_y <= '1';
+                             line_rstn      <= '1';
+                             line_enable    <= '0';
+                             col_rstn       <= '1';
+                             col_enable     <= '0';
+                             we             <= '0';
+                             timer_rstn     <= '0'; 
+                             timer_enable   <= '0';
+
+      when others         => proximo_estado <= inicio_partida;
+                             atualiza_pos_bola_x <= '0';
+                             atualiza_pos_bola_y <= '0';
+                             line_rstn      <= '1';
+                             line_enable    <= '0';
+                             col_rstn       <= '1';
+                             col_enable     <= '0';
+                             we             <= '0';
+                             timer_rstn     <= '1'; 
+                             timer_enable   <= '0';
+      
+    end case;
+  end process logica_mealy;
+  
+  -- purpose: Avança a FSM para o próximo estado
+  -- type   : sequential
+  -- inputs : CLOCK_50, rstn, proximo_estado
+  -- outputs: estado
+  seq_fsm: process (CLOCK_50, rstn)
+  begin  -- process seq_fsm
+    if rstn = '0' then                  -- asynchronous reset (active low)
+      estado <= inicio_partida;
+    elsif CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
+      estado <= proximo_estado;
+    end if;
+  end process seq_fsm;
+
+  -----------------------------------------------------------------------------
+  -- Processos do contador utilizado para atrasar a animação (evitar
+  -- que a atualização de quadros fique excessivamente veloz).
+  -----------------------------------------------------------------------------
+  -- purpose: Incrementa o contador a cada ciclo de clock
+  -- type   : sequential
+  -- inputs : CLOCK_50, timer_rstn
+  -- outputs: contador, timer
+  p_contador: process (CLOCK_50, timer_rstn)
+  begin  -- process p_contador
+    if timer_rstn = '0' then            -- asynchronous reset (active low)
+      contador <= 0;
+    elsif CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
+      if timer_enable = '1' then       
+        if contador = 1250000 - 1 then
+          contador <= 0;
+        else
+          contador <=  contador + 1;        
+        end if;
+      end if;
+    end if;
+  end process p_contador;
+
+  -- purpose: Calcula o sinal "timer" que indica quando o contador chegou ao
+  --          final
+  -- type   : combinational
+  -- inputs : contador
+  -- outputs: timer
+  p_timer: process (contador)
+  begin  -- process p_timer
+    if contador = 1250000 - 1 then
+      timer <= '1';
+    else
+      timer <= '0';
+    end if;
+  end process p_timer;
+
+  -----------------------------------------------------------------------------
+  -- Processos que sincronizam sinais assíncronos, de preferência com mais
+  -- de 1 flipflop, para evitar metaestabilidade.
+  -----------------------------------------------------------------------------
+  
+  -- purpose: Aqui sincronizamos nosso sinal de reset vindo do botão da DE1
+  -- type   : sequential
+  -- inputs : CLOCK_50
+  -- outputs: rstn
+--  build_rstn: process (CLOCK_50)
+--    variable temp : std_logic;          -- flipflop intermediario
+--  begin  -- process build_rstn
+--    if CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
+--      rstn <= temp;
+--      temp := KEY(0);      
+--    end if;
+--  end process build_rstn;
+
+  
+end comportamento;
