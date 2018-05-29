70a37367ca060e55fe92075d37aeb809d1b579d5
MelloFelipe <39704657+MelloFelipe@users.noreply.github.com>
2018-05-29 18:01:35 -0300

    Update UC.vhd

--- UC.vhd
+++ UC.vhd
@@ -4,9 +4,9 @@ use ieee.std_logic_1164.all;
 entity UC is
   port (    
     CLOCK_50                  : in  std_logic;
-    --KEY                       : in  std_logic_vector(0 downto 0);
-    key_on							: in	std_logic_vector(2 downto 0);
-    key_code						: in	std_logic_vector(47 downto 0);
+  --KEY                       : in  std_logic_vector(0 downto 0);
+    key_on                    : in	std_logic_vector(2 downto 0);
+    key_code                  : in	std_logic_vector(47 downto 0);
     VGA_R, VGA_G, VGA_B       : out std_logic_vector(7 downto 0);
     VGA_HS, VGA_VS            : out std_logic;
     VGA_BLANK_N, VGA_SYNC_N   : out std_logic;
