ae32137fcf046c1c498947d10d615bd794cb5f22
MelloFelipe <39704657+MelloFelipe@users.noreply.github.com>
2018-06-04 11:04:08 -0300

    Update UC.vhd

--- UC.vhd
+++ UC.vhd
@@ -44,13 +44,13 @@ architecture comportamento of UC is
   -- Sinais que armazem a posição de uma bola, que deverá ser desenhada
   -- na tela de acordo com sua posição.
 
-  signal pos_bola_x : integer range 0 to 127;  -- coluna atual da bola
-  signal pos_bola_y : integer range 0 to 95;   -- linha atual da bola
+  signal pos_bola_x : integer range 0 to 127 := 64;  -- coluna atual da bola
+  signal pos_bola_y : integer range 0 to 95  := 80;  -- linha atual da bola
 
   signal atualiza_pos_bola_x : std_logic;    -- se '1' = bola muda sua pos. no eixo x
   signal atualiza_pos_bola_y : std_logic;    -- se '1' = bola muda sua pos. no eixo y
 
-  signal derivada_bola : integer range -3 to 3; -- angulo da direcao da bola
+  signal derivada_bola : integer range -3 to 3 := 1; -- angulo da direcao da bola
   -- derivada_bola_bola = 0: angulo de 0            graus em relacao ao eixo x
   -- derivada_bola_bola = 1: angulo de +-45         graus em relacao ao eixo x
   -- derivada_bola_bola = 2: angulo de +-63.435     graus em relacao ao eixo x
@@ -66,7 +66,7 @@ architecture comportamento of UC is
   -- veloz. Aqui utilizamos um contador de 0 a 1250000, de modo que quando
   -- alimentado com um clock de 50MHz, ele demore 25ms (40fps) para contar até o final.
   
-  signal contador : integer range 0 to 1250000 - 1;  -- contador
+  signal contador : integer range 0 to 1250000 - 1 := 0;  -- contador
   signal timer : std_logic;        -- vale '1' quando o contador chegar ao fim
   signal timer_rstn, timer_enable : std_logic;
   
@@ -74,19 +74,21 @@ architecture comportamento of UC is
 
   -- Sinais para controlar o movimento dos PAD's
   -- PAD tem o seu tamanho definido como 7 pixels
-  signal pos_PAD1 : integer range 3 to 92;   -- linha atual do pixel central do PAD1
-  signal pos_PAD2 : integer range 3 to 92;   -- linha atual do pixel central do PAD2
+  signal pos_PAD1 : integer range 3 to 92 := 47;   -- linha atual do pixel central do PAD1
+  signal pos_PAD2 : integer range 3 to 92 := 47;   -- linha atual do pixel central do PAD2
 
   signal atualiza_pos_PADs : std_logic;    -- se '1' = PAD1 e PAD2 muda sua pos. no eixo y
   
   signal ponto: std_logic := '0'; -- '1' quando um ponto foi marcado
-  signal pontos_PAD1 : integer range 0 to 7;   -- pontos marcados pelo PAD1
-  signal pontos_PAD2 : integer range 0 to 7;   -- pontos marcados pelo PAD2
+  signal pontos_PAD1 : integer range 0 to 7 := 0;   -- pontos marcados pelo PAD1
+  signal pontos_PAD2 : integer range 0 to 7 := 0;   -- pontos marcados pelo PAD2
   
+  signal flag_inicio : std_logic := '0'; -- verifica se o jogo foi iniciado
+  signal flag_inicio_rstn : std_logic := '1'; -- reseta valor da flag, ativo em baixo
   -- O QUE FALTA:
   -- Implementar a contrucao de todas as telas (inicio, partida, reseta_partida e game_over), talvez a construcao da bola e dos PADs ja esteja 10/10
   
-  -- ideia para deixar mais bonito: na maquina de estados, zerar tudo antes do case e so alterar para 1 dentro do case (igual ao lab10)
+  -- ideia para deixar mais bonito: na maquina de estados, zerar tudo antes do case e so alterar para 1 dentro do case (igual ao lab10), na verdade talvez isso de problemas
   
 begin  -- comportamento
 
@@ -167,10 +169,26 @@ begin  -- comportamento
   -- possamos avançar para o próximo estado.
   fim_escrita <= '1' when (line = 95) and (col = 127)
                  else '0'; 
-
+				 
+  -----------------------------------------------------------------------------
+  -- Processo que verifica se o jogo deve ser iniciado
+  verifica_inicio: process(CLOCK_50)
+  begin
+	if rising_edge(CLOCK_50) then
+		if flag_inicio_rstn = '0' then
+			flag_inicio <= '0';
+		end if;
+		if flag_inicio <= '0' then
+			if key_code(15 downto 0) = x"005A" then -- se a tecla "ENTER" for pressionada
+				flag_inicio <= '1';
+			end if;
+		end if;
+	end if;
+  end process verifica_inicio;
+  -----------------------------------------------------------------------------
   -----------------------------------------------------------------------------
   -- Abaixo estão processos relacionados com a atualização da posição da
-  -- bola. Todos são controlados por sinais de enable de modo que a posição
+  -- bola e dos PAD's. Todos são controlados por sinais de enable de modo que a posição
   -- só é de fato atualizada quando o controle (uma máquina de estados)
   -- solicitar.
   -----------------------------------------------------------------------------
@@ -231,7 +249,7 @@ begin  -- comportamento
     variable direcao : direcao_t := desce;
   begin  -- process p_atualiza_pos_bola_x
     if rstn = '0' then                  -- asynchronous reset (active low)
-      pos_bola_y <= 0;
+      pos_bola_y <= 0; -- talvez mudar para 80
     elsif CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
       if atualiza_pos_bola_y = '1' then
         if direcao = desce then         
@@ -273,7 +291,7 @@ begin  -- comportamento
   -- type   : sequential
   -- inputs : CLOCK_50
   -- outputs: pos_PAD1 e pos_PAD2
-  p_atualiza_pos_PADs: process (CLOCK_50)
+  p_atualiza_pos_PADs: process (CLOCK_50) -- talvez mudar para key_on
   begin
     if CLOCK_50'event and CLOCK_50 = '1' then  -- rising clock edge
       if atualiza_pos_PADs = '1' then
@@ -333,9 +351,29 @@ begin  -- comportamento
   -- inputs : estado, fim_escrita, timer
   -- outputs: proximo_estado, atualiza_pos_bola_x, atualiza_pos_bola_y, line_rstn,
   --          line_enable, col_rstn, col_enable, we, timer_enable, timer_rstn
-  logica_mealy: process (estado, fim_escrita, timer)
+  logica_mealy: process (estado, fim_escrita, timer, flag_inicio)
   begin  -- process logica_mealy
     case estado is
+		when inicio_jogo    => if timer = '1' then
+									    if flag_inicio = '1' then
+										   proximo_estado <= inicio_partida;
+									    else
+                                 proximo_estado <= constroi_quadro;
+										 end if;
+                             else
+                               proximo_estado <= inicio_jogo;
+                             end if;
+                             atualiza_pos_bola_x <= '0';
+                             atualiza_pos_bola_y <= '0';
+									  atualiza_pos_PADs   <= '0';
+                             line_rstn      <= '0';  -- reset é active low!
+                             line_enable    <= '0';
+                             col_rstn       <= '0';  -- reset é active low!
+                             col_enable     <= '0';
+                             we             <= '0';
+                             timer_rstn     <= '1';  -- reset é active low!
+                             timer_enable   <= '1';
+									  
       when inicio_partida    => if timer = '1' then              
                                proximo_estado <= constroi_quadro;
                              else
